52d681a044fb36b8a52433bee8221fc78083f763
Hoid <tylerlcheek@gmail.com>
2025-08-21 10:21:21 -0400

    Implement a basic line chart with real pathogen data

--- src/components/explorer/visualization_editor.py
+++ src/components/explorer/visualization_editor.py
@@ -1,67 +1,123 @@
-from dash import callback, dcc, Input, Output, State
+import json
+from dash import callback, dcc, Input, Output, State, html
 import dash_mantine_components as dmc
 from dash_iconify import DashIconify
 from .controls import controls
+import pandas as pd
 
-controls_visibility_store = dcc.Store(id='controls-visibility', data=True)  # True = open, False = closed
+controls_visibility_store = dcc.Store(
+    id="controls-visibility", data=True
+)  # True = open, False = closed
 
 controls_toggle = dmc.Button(
-  'Hide Controls',
-  rightSection=DashIconify(icon='feather:chevron-right'),
-  id='controls-toggle',
-  variant='subtle',
-  size='xs',
+    "Hide Controls",
+    rightSection=DashIconify(icon="feather:chevron-right"),
+    id="controls-toggle",
+    variant="subtle",
+    size="xs",
 )
 
-def visualization_editor(image_url='https://placehold.co/1200x400'):
-  return dmc.Grid(
-    children=[
-      dmc.GridCol(
-        dmc.Image(id='insight-visualization', src=image_url, radius='sm'),
-        id='visualization-column',
-        span=8,
-      ),
-      dmc.GridCol(
-        controls,
-        id='controls-column',
-        span=4,
-      ),
-      dmc.GridCol(
-        [
-          controls_visibility_store,
-          dmc.ButtonGroup([
-            controls_toggle,
-          ], style=dict(justifyContent='flex-end')),
+with open("src/config/constant.json", "r") as f:
+    constants = json.load(f)
+with open("src/config/viz_settings.json", "r") as f:
+    viz_settings = json.load(f)
+
+df = pd.read_csv("src/assets/round1/cum-hosp/California/quantile/part-0.csv")
+df = df.set_index("target_end_date")
+df = df[(df["scenario_id"] == 2)]
+df = df[df["type_id"] == 0.5]
+df = df[df["model_name"] == 1]
+df = df[df["age_group"] == "0-130"]
+
+title = (
+    constants["pathogen_display_name"]
+    + " Scenario "
+    + constants["scenario_id"]["1"]
+    + " using "
+    + constants["model_name"]["1"]
+    + " model, for "
+    + viz_settings["Round 1 - 2023/2024"]["age_group"]["0-130"]
+)
+
+
+def visualization_editor():
+    return dmc.Grid(
+        children=[
+            dmc.GridCol(
+                html.Div(
+                    [
+                        dmc.Title(title, order=1),
+                        dmc.LineChart(
+                            h=300,
+                            dataKey="target_end_date",
+                            data=df.to_dict(orient="records"),
+                            tickLine="xy",
+                            yAxisProps={"tickMargin": 15, "orientation": "right"},
+                            xAxisProps={"tickMargin": 15, "orientation": "top"},
+                            series=[
+                                {
+                                    "name": "value",
+                                    "label": "Cumulative Hospitalizations",
+                                    "color": "indigo.6",
+                                },
+                            ],
+                        ),
+                    ]
+                ),
+                id="visualization-column",
+                span=8,
+            ),
+            dmc.GridCol(
+                controls,
+                id="controls-column",
+                span=4,
+            ),
+            dmc.GridCol(
+                [
+                    controls_visibility_store,
+                    dmc.ButtonGroup(
+                        [
+                            controls_toggle,
+                        ],
+                        style=dict(justifyContent="flex-end"),
+                    ),
+                ],
+                span=dict(base=12),
+            ),
         ],
-        span=dict(base=12),
-      ),
-    ],
-    mb=12,
-  )
+        mb=12,
+    )
+
 
 # toggle the controls visibility store value when clicking the button
 @callback(
-  Output('controls-visibility', 'data'),
-  Output('controls-toggle', 'children'),
-  Output('controls-toggle', 'rightSection'),
-  Input('controls-toggle', 'n_clicks'),
-  State('controls-visibility', 'data'),
-  prevent_initial_call=True
+    Output("controls-visibility", "data"),
+    Output("controls-toggle", "children"),
+    Output("controls-toggle", "rightSection"),
+    Input("controls-toggle", "n_clicks"),
+    State("controls-visibility", "data"),
+    prevent_initial_call=True,
 )
 def toggle_controls_visibility_store(n_clicks, is_open):
-  new_open = not is_open
-  new_label = 'Hide Controls' if new_open else 'Show Controls'
-  new_icon = DashIconify(icon='feather:chevron-right') if new_open else DashIconify(icon='feather:chevron-left')
-  return new_open, new_label, new_icon
+    new_open = not is_open
+    new_label = "Hide Controls" if new_open else "Show Controls"
+    new_icon = (
+        DashIconify(icon="feather:chevron-right")
+        if new_open
+        else DashIconify(icon="feather:chevron-left")
+    )
+    return new_open, new_label, new_icon
+
 
 # update layout based on store value
 @callback(
-  Output('visualization-column', 'span'),
-  Output('controls-column', 'span'),
-  Output('controls-column', 'style'),
-  Input('controls-visibility', 'data'),
+    Output("visualization-column", "span"),
+    Output("controls-column", "span"),
+    Output("controls-column", "style"),
+    Input("controls-visibility", "data"),
 )
 def update_sidebar_display(is_open):
-  if is_open:
-    return 8, 4, {}
-  else: return 12, 0, {'display': 'none'}
+    if is_open:
+        return 8, 4, {}
+    else:
+        return 12, 0, {"display": "none"}
--- src/pages/explorer.py
+++ src/pages/explorer.py
@@ -1,4 +1,15 @@
-from dash import callback, ctx, dcc, exceptions, html, Input, no_update, Output, register_page, State
+from dash import (
+    callback,
+    ctx,
+    dcc,
+    exceptions,
+    html,
+    Input,
+    no_update,
+    Output,
+    register_page,
+    State,
+)
 import dash_mantine_components as dmc
 from urllib.parse import parse_qs
 from dash_iconify import DashIconify
@@ -7,51 +18,49 @@ from src.data.templates import templates
 from src.components.explorer import save_button, visualization_editor
 from src.util.get_query_param import get_query_param
 
-register_page(__name__, path_template='/explorer', name='Insight Explorer')
+register_page(__name__, path_template="/explorer", name="Insight Explorer")
 
-back_button = dmc.Anchor('← Abandon Changes', href='/', id='back-button')
+back_button = dmc.Anchor("← Abandon Changes", href="/", id="back-button")
 
 reset_button = dcc.Link(
-  dmc.Button(
-    'Reset to Original',
-    leftSection=DashIconify(icon='feather:refresh-ccw'),
-    variant='outline',
-  ),
-  id='reset-button',
-  href='#',
+    dmc.Button(
+        "Reset to Original",
+        leftSection=DashIconify(icon="feather:refresh-ccw"),
+        variant="outline",
+    ),
+    id="reset-button",
+    href="#",
 )
 
 toolbar = dmc.Flex(
-  children=[
-    back_button,
-    dmc.Group([reset_button, save_button])
-  ],
-  justify='space-between',
-  align='center',
-  mb=24,
+    children=[back_button, dmc.Group([reset_button, save_button])],
+    justify="space-between",
+    align="center",
+    mb=24,
 )
+
 
 def layout(starter=None):
-  item = get_insight(starter) or {}
-  details = item.get('details', '')
-  title = item.get('title', '')
-  overview = item.get('overview', '')
-  image_url = item.get('image_url', '')
+    item = get_insight(starter) or {}
+    details = item.get("details", "")
+    title = item.get("title", "")
+    overview = item.get("overview", "")
 
-  return dmc.Container(
-    [
-      toolbar,
-      visualization_editor(image_url),
-    ],
-    fluid=True,
-  )
+    return dmc.Container(
+        [
+            toolbar,
+            visualization_editor(),
+        ],
+        fluid=True,
+    )
+
 
 @callback(
-  Output('back-button', 'href'),
-  Input('url', 'search'),
+    Output("back-button", "href"),
+    Input("url", "search"),
 )
 def update_back_button_href(search):
-  starter = get_query_param(search, 'starter')
-  if not starter:
-    raise exceptions.PreventUpdate
-  return f'/viewer?id={starter}' if starter else '/'
+    starter = get_query_param(search, "starter")
+    if not starter:
+        raise exceptions.PreventUpdate
+    return f"/viewer?id={starter}" if starter else "/"
