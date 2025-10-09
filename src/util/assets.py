20957c65c272f9a054e6d108000726a8d30e29fa
Matt Watson <mwatson@renci.org>
2025-10-09 19:18:07 -0400

    use assets util from feature/pdf-export

--- src/util/assets.py
+++ src/util/assets.py
@@ -1,12 +1,19 @@
 import os
+import base64
+import mimetypes
 
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
 
-
 def asset_uri(relative_path: str) -> str:
-  """
-  build a URI for an asset inside the project root assets/ dir,
-  e.g., asset_uri('images/covid19-smh-logo.png')
-  """
+  '''
+  Return a data: URI for an asset inside assets/.
+  Example: asset_uri('images/covid19-smh-logo.png')
+  '''
   full_path = os.path.join(PROJECT_ROOT, 'assets', relative_path)
-  return f'file://{full_path}'
+  mime_type, _ = mimetypes.guess_type(full_path)
+  mime_type = mime_type or 'application/octet-stream'
+
+  with open(full_path, 'rb') as f:
+    encoded = base64.b64encode(f.read()).decode('utf-8')
+
+  return f'data:{mime_type};base64,{encoded}'
