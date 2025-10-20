c8039b43110b734f8146f42b9af8aa5b3b63d0c7
Matt Watson <mwatson@renci.org>
2025-10-20 16:04:31 -0400

    tweak layout and control breakpoints

--- src/components/viz_editor/controls/zoom_control.py
+++ src/components/viz_editor/controls/zoom_control.py
@@ -25,14 +25,13 @@ def zoom_control(value=None):
         justify='space-between',
       ),
       dmc.Divider(),
-      dmc.Flex(
+      dmc.Grid(
         [
-          dmc.DateInput(id='zoom-x-min', label='X min', value=x_min),
-          dmc.DateInput(id='zoom-x-max', label='X max', value=x_max),
-          dmc.NumberInput(id='zoom-y-min', label='Y min', value=y_min, step=100),
-          dmc.NumberInput(id='zoom-y-max', label='Y max', value=y_max, step=100),
+          dmc.GridCol(dmc.DateInput(id='zoom-x-min', label='X min', value=x_min), span=dict(base=12, md=6, sm=6)),
+          dmc.GridCol(dmc.DateInput(id='zoom-x-max', label='X max', value=x_max), span=dict(base=12, md=6, sm=6)),
+          dmc.GridCol(dmc.NumberInput(id='zoom-y-min', label='Y min', value=y_min, step=100), span=dict(base=12, md=6, sm=6)),
+          dmc.GridCol(dmc.NumberInput(id='zoom-y-max', label='Y max', value=y_max, step=100), span=dict(base=12, md=6, sm=6)),
         ],
-        gap='sm',
       ),
     ]
   )
