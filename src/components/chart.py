95f68534cf7585ccaa7e4b8b0ed8d4bb416e8a3f
Matt Watson <mwatson@renci.org>
2025-11-07 15:45:25 -0500

    remove unnecessary change

--- src/components/chart.py
+++ src/components/chart.py
@@ -588,7 +588,6 @@ class Chart:
           line_color=annotation.color,
           line_width=1,
           annotation_text=annotation.label,
-          name=annotation.label,
         )
       elif isinstance(annotation, VerticalAnnotation):
         # Convert date to timestamp in milliseconds, as there passing the datetime object directly and adding annotation_text causes a TypeError
