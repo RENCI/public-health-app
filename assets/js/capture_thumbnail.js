42afe3d2d5f839c54038dd7172abf0ae9cc75005
Matt Watson <mwatson@renci.org>
2025-11-18 15:40:44 -0500

    update selector, format, scale

--- assets/js/capture_thumbnail.js
+++ assets/js/capture_thumbnail.js
@@ -1,22 +1,24 @@
 (function () {
+  const selector = '#graph .js-plotly-plot';
+
   window.dash_clientside = Object.assign({}, window.dash_clientside, {
     clientside: {
       async capture_thumbnail(n) {
         if (!n) return window.dash_clientside.no_update;
 
-        const plot = document.querySelector('#graph .js-plotly-plot');
+        const plot = document.querySelector(selector);
 
         if (!plot) {
-          console.warn('nothing found with selector "#graph .js-plotly-plot"');
+          console.warn(`nothing found with selector "${selector}"`);
           return window.dash_clientside.no_update;
         }
 
         try {
           const img = await Plotly.toImage(plot, {
-            format: 'png',
+            format: 'svg',
             width: 1000,
             height: 800,
-            scale: 2,
+            scale: 1,
           });
           return img;
         } catch (error) {
