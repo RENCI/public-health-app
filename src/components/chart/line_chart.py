137a71ea8dae8a4c0cfc50529d394554516dc0f9
Hoid <tylerlcheek@gmail.com>
2025-11-19 10:34:04 -0500

    Add letter prefix to subplot titles in line chart

--- src/components/chart/line_chart.py
+++ src/components/chart/line_chart.py
@@ -68,7 +68,7 @@ class LineChart(Chart):
       cols=1,
       vertical_spacing=vertical_spacing,
       row_heights=[1] * num_rows,
-      subplot_titles=[f'{s.description}' for s in self.controls.scenarios],
+      subplot_titles=[f'{s.name.split("-")[0]}. {s.description}' for s in self.controls.scenarios],
     )
 
     # add traces for each scenario
