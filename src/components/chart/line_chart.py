24e20b18f69282e41061218ef4f7755ff3977486
Matt Watson <mwatson@renci.org>
2025-12-10 12:20:13 -0500

    fix merge conflicts

--- src/components/chart/line_chart.py
+++ src/components/chart/line_chart.py
@@ -290,10 +290,8 @@ class LineChart(Chart):
     )
     self.refresh_fig()
 
-  def _plot_uncertainty_interval(self, scenario_df: pd.DataFrame, model: Model, row_num: int):
-    all_bounds = self.controls.uncertainty_interval.get_bounds()
-    for i, bounds in enumerate(all_bounds):   
-      lower_q, upper_q = bounds   
+  def _plot_uncertainty_interval(self, scenario_df: pd.DataFrame, model: Model, row_num: int, col_num: int):
+    for lower_q, upper_q in self.controls.uncertainty_interval.get_bounds():
       lower_model = scenario_df.query('type_id == @lower_q and model_name == @model.id')
       upper_model = scenario_df.query('type_id == @upper_q and model_name == @model.id')
       fill_color = get_model_color_with_uncertainty_interval(
@@ -308,15 +306,14 @@ class LineChart(Chart):
           fill='toself',
           fillcolor=fill_color,
           line=dict(color='rgba(0,0,0,0)'),
-          name=f'{model.name}',
-          legendgroup=f'{model.name}',
+          legendgroup=f'Model {model.name}',
           hoverinfo='skip',
-          showlegend=(row_num == 1 and i == len(all_bounds) - 1),
+          showlegend=False,
         ),
         row=row_num,
         col=col_num,
       )
-
+  
   def _reload_data(self):
     """
     Reload data, for example if a variable changed, e.g. round number, location, target, etc.
