a879165cd4d59d35bc07f94adfd336857c19f58f
Matt Watson <mwatson@renci.org>
2025-12-11 10:24:37 -0500

    create collapsible card component

---
+++ src/components/collapsible_card/collapsible_card.py
@@ -0,0 +1,54 @@
+from typing import Any, Dict
+
+import dash_mantine_components as dmc
+from dash_iconify import DashIconify
+
+from src.components.collapsible_card.callbacks import CollapsibleCardCallbacks
+from src.components.collapsible_card.store import CollapsibleCardStore
+
+
+# ensure callbacks register only once
+_callbacks = CollapsibleCardCallbacks()
+
+
+class CollapsibleCard:
+  def __init__(
+    self,
+    title: str,
+    children: Any,
+    id: Dict[str, Any],
+    initial_state: bool = True,
+  ) -> None:
+    self.id = id
+
+    self.store = CollapsibleCardStore(
+      id=self.id, 
+      initial_state=initial_state
+    )
+
+    self._layout = dmc.Card(
+      [
+        dmc.Group(
+          children=[
+            dmc.Text(title, fw=500),
+            dmc.ActionIcon(
+              DashIconify(icon='feather:chevron-up'),
+              variant='transparent',
+              id={'type': 'collapsible-card-toggle', 'index': self.id['index']},
+            ),
+          ],
+          justify='space-between',
+        ),
+        dmc.Collapse(
+          children,
+          id={'type': 'collapse', 'index': self.id['index']},
+          opened=initial_state,
+        ),
+        self.store.layout,
+      ],
+      variant='soft',
+    )
+
+  @property
+  def layout(self):
+    return self._layout
---
+++ src/components/collapsible_card/store.py
@@ -0,0 +1,27 @@
+import dash
+from dash import dcc
+from typing import Dict, Any
+
+
+class CollapsibleCardStore:
+  STORE_TYPE = 'collapsible-card-store'
+
+  def __init__(self, id: Dict[str, Any], initial_state: bool = True):
+    self.id = {'type': self.STORE_TYPE, 'index': id['index']}
+    self.initial_state = initial_state
+
+  @staticmethod
+  def get_id(match_all=False):
+    if match_all:
+      return {'type': CollapsibleCardStore.STORE_TYPE, 'index': dash.ALL}
+    return {'type': CollapsibleCardStore.STORE_TYPE, 'index': dash.MATCH}
+
+  @property
+  def layout(self):
+    return dcc.Store(
+      id=self.id,
+      data={
+      'id': self.id,
+      'isOpen': self.initial_state,
+      },
+    )
